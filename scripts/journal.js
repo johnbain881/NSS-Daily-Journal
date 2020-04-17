c715b8f173e1ddf0db9dc94075db77c4beb2c584
John Bain <johnknoxbain@gmail.com>
2020-04-17 13:41:06 -0500

    Added JS object to hold entries

---
+++ scripts/journal.js
@@ -0,0 +1,29 @@
+const journalEntry = {
+    date: "4/17/20",
+    entryTitle: "JavaScript",
+    entry: "We have officially started JavaScript!",
+    mood: "Happy"
+}
+
+let entries = [];
+entries.push(journalEntry);
+
+const journalEntry1 = {
+    date: "4/17/20",
+    entryTitle: "Finally!",
+    entry: "I have been anticipating starting JavaScript, because it is so much fun!",
+    mood: "Motivated"
+}
+
+entries.push(journalEntry1);
+
+
+const journalEntry2 = {
+    date: "4/17/20",
+    entryTitle: "Long Day",
+    entry: "I woke up pretty early this morning, and I'm pretty tired.",
+    mood: "Tired"
+}
+
+entries.push(journalEntry2);
+console.log(entries)
